f07b3e3e6f6bb07ef4ff46ec57160ed2b5bb6289
andrewdemaris <47347848+andrewdemaris@users.noreply.github.com>
2021-12-09 02:06:29 -0800

    ugh it works but i hate it

---
+++ 2d_game/src/Board.java
@@ -0,0 +1,92 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.*;
+
+public class Board extends JPanel implements ActionListener, KeyListener {
+
+    // controls the delay between each tick in ms
+    private final int DELAY = 25;
+    // controls the size of the board
+    public static final int TILE_SIZE = 25;
+    public static final int ROWS = 20;
+    public static final int COLUMNS = 20;
+
+    // suppress serialization warning
+    private static final long serialVersionUID = 490905409104883233L;
+
+    // keep a reference to the timer object that triggers actionPerformed() in
+    // case we need access to it in another method
+    private Timer timer;
+    // objects that appear on the game board
+    private Player player;
+
+    public Board() {
+        // set the game board size
+        setPreferredSize(new Dimension(TILE_SIZE * COLUMNS, TILE_SIZE * ROWS));
+        // set the game board background color
+        setBackground(new Color(22, 78, 1, 126));
+
+        // initialize the game state
+        player = new Player();
+
+        // this timer will call the actionPerformed() method every DELAY ms
+        timer = new Timer(DELAY, this);
+        timer.start();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // this method is called by the timer every DELAY ms.
+        // use this space to update the state of your game or animation
+        // before the graphics are redrawn.
+
+        // prevent the player from disappearing off the board
+        player.tick();
+
+
+        // calling repaint() will trigger paintComponent() to run again,
+        // which will refresh/redraw the graphics.
+        repaint();
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        // when calling g.drawImage() we can use "this" for the ImageObserver
+        // because Component implements the ImageObserver interface, and JPanel
+        // extends from Component. So "this" Board instance, as a Component, can
+        // react to imageUpdate() events triggered by g.drawImage()
+
+        // draw our graphics.
+
+
+        player.draw(g, this);
+
+        // this smooths out animations on some systems
+        Toolkit.getDefaultToolkit().sync();
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // this is not used but must be defined as part of the KeyListener interface
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // react to key down events
+        player.keyPressed(e);
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // react to key up events
+    }
+
+
+
+
+
+
+}
